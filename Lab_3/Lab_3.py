7412a4cc40aa74a2e4d460f1baf62a2d16396a7a
Александр Комаров <raptor.al.com@yandex.ru>
2020-12-17 12:05:03 +0300

    долбежка с третьей лабой. Отчет по первой лабе

--- Lab_3/Lab_3.py
+++ Lab_3/Lab_3.py
@@ -1,77 +1,109 @@
-import pandas, numpy
+import pandas, seaborn, numpy
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold
-from pandas.plotting import scatter_matrix
-from sklearn.preprocessing import OneHotEncoder
-
+import additional_func as af
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
 
 pandas.set_option('display.max_columns', None)
-DataFrame = pandas.read_csv('AB_NYC_2019.csv')
+DataFrame = pandas.read_csv('train.csv')
+
+# Получение таблицы с признаками, их типом, максимальным и минимальным значениями
+DataFrame.describe().to_excel('data_describe.xlsx')
+
+# Удаление столбцов с большим кол-вом пустых значений
+DataFrame = DataFrame.drop(['PoolQC', 'Alley', 'FireplaceQu', 'Fence', 'MiscFeature', 'LotFrontage'], axis=1)
+# Фильтрация выбросов
+DataFrame = DataFrame[(DataFrame[["GrLivArea"]] < 4000).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["GarageCars"]] < 4).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["OverallQual"]] > 2).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["1stFlrSF"]] < 2000).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["GrLivArea"]] < 2500).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["TotalBsmtSF"]] < 2000).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["TotalBsmtSF"]] > 450).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["GarageArea"]] < 1000).all(axis=1)]
+
+
+# Удаление нулей и обновление индексов
 DataFrame = DataFrame.dropna()
+DataFrame.reset_index(drop=True)
 
-unique_type_of_room = DataFrame['room_type'].unique()
-index_for_unique_type_of_room = [2, 3, 1]
+print("Dataframe size = ", len(DataFrame))
 
+for feature in DataFrame.columns:
+    if DataFrame[feature].dtype == object:
+        DataFrame[feature] = pandas.Categorical(DataFrame[feature])
+        sex_map_train = dict(zip(DataFrame[feature].cat.codes, DataFrame[feature]))
+        DataFrame[feature] = DataFrame[feature].cat.codes
 
-def get_index(line, index_mas):
-    for i in range(len(unique_type_of_room)):
-        if line == unique_type_of_room[i]:
-            return index_mas[i]
+# Вывод значений корреляции всех признаков с ценой в табличку
+corr = DataFrame[['SalePrice'] + DataFrame.columns.to_list()].corr().iloc[0]
+corr.sort_values().to_excel('correlation.xlsx')
 
+# Разбиение цен на группы и создание нового столбца
+# labels = [1, 2, 3]
+# price_group = pandas.cut(DataFrame['SalePrice'],
+#                     bins=[DataFrame['SalePrice'].min(),
+#                           145000,
+#                           200000,
+#                           DataFrame['SalePrice'].max()],
+#                     labels=labels)
+# DataFrame.loc[:, 'price_group'] = numpy.array(price_group)
+# af.build_bar_chart(DataFrame, 'price_group')
 
-DataFrame.loc[:, 'type_of_room'] = \
-    DataFrame['room_type'].apply(lambda x: get_index(str(x), index_for_unique_type_of_room))
-
-
-#DataFrame.describe()
-#DataFrame.mean()
-#DataFrame.groupby()
-
-attributes = ["neighbourhood_group", "neighbourhood", "room_type", "price", "minimum_nights",\
-              "number_of_reviews", "last_review", "reviews_per_month"]
-
-scatter_matrix(DataFrame[attributes])
-
-DataFrame = DataFrame.query("price > 0 and price < 800")
-DataFrame = DataFrame.query("reviews_per_month < 30")
-DataFrame = DataFrame.query("minimum_nights < 29")
-DataFrame = DataFrame.query("calculated_host_listings_count < 50")
-
-
-def build_gistogramm(feature):
-    plt.figure(num='plt.bar result' + feature)
-    counts_neighb_gr = DataFrame[feature].value_counts()
-    plt.bar(counts_neighb_gr.index, counts_neighb_gr.values)
-    plt.xticks(rotation=30)
-
-
-build_gistogramm('neighbourhood_group')
-build_gistogramm('room_type')
-build_gistogramm('price')
-build_gistogramm('minimum_nights')
-build_gistogramm('reviews_per_month')
-
-scatter_matrix(DataFrame[attributes])
+# Дополнительное удаление пустых строк. Почему то это нужно сделать еще раз, иначе бо-бо
+DataFrame = DataFrame.dropna()
+# ИТОГОВЫЙ НАБОР ПРИЗНАКОВ
+attributes = ['GarageCars', 'GrLivArea', 'OverallQual', 'TotalBsmtSF', 'GarageArea', '1stFlrSF',
+              'FullBath', 'TotRmsAbvGrd', 'YearBuilt',
+              'ExterQual', 'KitchenQual', 'BsmtQual', 'GarageFinish', 'HeatingQC', 'GarageType']
+              #  'GrLivArea', 'OverallQual', 'TotalBsmtSF', 'GarageArea', '1stFlrSF'
+              # 'FullBath', 'TotRmsAbvGrd', 'YearBuilt'
+              # 'ExterQual', 'KitchenQual', 'BsmtQual', 'GarageFinish', 'HeatingQC', 'GarageType']
+# ДАТАФРЕЙМ СО ВСЕМИ ЧИСЛЕННЫМИ ПРИЗНАКАМИ
+data_num = DataFrame[attributes]
+# ЦЕЛЕВОЙ ПРИЗНАК
+df_target = DataFrame['SalePrice']
 
 
+# Отрисовка гистограмм всех признаков из итогового набора
+# for i in attributes:
+#     af.build_bar_chart(data_num, i)
 
-data_cat = DataFrame["neighbourhood_group"]
-data_cat_encoder, data_categories = data_cat.factorize()
+# Построение карты корреляций
+# plt.figure()
+# corr = DataFrame[attributes + ['SalePrice']].corr()
+# sns_hmap = seaborn.heatmap(abs(corr))
+# sns_hmap.set_title("correlation PANDAS + SEABORN")
 
-encoder = OneHotEncoder()
-data_cat_1hot = encoder.fit_transform(data_cat_encoder.reshape(-1, 1))
+# таблицы для записи итоговых результатов
+result = pandas.DataFrame()
+time_result = pandas.DataFrame()
 
-print(data_cat_1hot.toarray())
+# ОБУЧЕНИЕ МОДЕЛЕЙ
+# Оригинальные данные
+pca = PCA(n_components=1)
+data = pca.fit_transform(data_num)
+af.get_analiz(data_num, df_target, result, time_result, 'original', 0)
+# af.scatter_plot_func(DataFrame, data_num, 'SalePrice', "ORIGINAL")
+# Стандартизация
+df_stand = preprocessing.scale(data_num)
+df_stand = pandas.DataFrame(data=df_stand, index=data_num.index, columns=attributes)
+data = pca.fit_transform(df_stand)
+af.get_analiz(pandas.DataFrame(data), df_target, result, time_result, 'standardization', 1)
+# af.scatter_plot_func(DataFrame, df_stand, 'SalePrice', "STANDARDIZATION")
+# Нормализация
+df_norm = preprocessing.normalize(data_num)
+df_norm = pandas.DataFrame(data=df_norm, index=data_num.index, columns=attributes)
+data = pca.fit_transform(df_norm)
+af.get_analiz(pandas.DataFrame(data), df_target, result, time_result, 'normalization', 2)
+# af.scatter_plot_func(DataFrame, df_norm, 'SalePrice', "NORMALIZATION")
 
-DataFrame.loc[:, ['name1', 'name2', 'name3', 'name4', 'name5']] = data_cat_1hot.toarray()
+# Вывод в таблицы excel
+# result.to_excel('result.xlsx')
+# time_result.to_excel('time_result.xlsx')
+#
+# print(result)
+# print(time_result)
 
-attributes = ["neighbourhood_group", "neighbourhood", "room_type", "price", "minimum_nights",\
-              "number_of_reviews", "last_review", "reviews_per_month", "type_of_room", \
-              "name1", "name2", "name3", "name4", "name5"]
-
-scatter_matrix(DataFrame[attributes])
-
-
-
-
-plt.show()
+# plt.show()
+pandas.reset_option('max_columns')
--- Lab_3/additional_func.py
+++ Lab_3/additional_func.py
@@ -1,30 +1,48 @@
 import matplotlib.pyplot as plt
+import numpy, pandas
+from timeit import default_timer as timer
 from matplotlib import cm
-"""
-Набор дополнительных функций для лабораторной работы 3 'Регрессия'
-"""
-
-
-"""
-Функция для перевода категориального признкака в численный в лоб 
-благодаря массиву с индексами
-"""
-def get_index(line, index_mas, unique_signs):
-    for i in range(len(unique_signs)):
-        if line == unique_signs[i]:
-            return index_mas[i]
+from sklearn.model_selection import KFold
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression, ElasticNet
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import GridSearchCV
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import style
+style.use('ggplot')
 
 """
 Построение гистограммы распределения по входному признаку датафрейма
 """
-def build_gistogramm(feature, data):
-    plt.figure(num='plt.bar result' + feature)
-    counts = data[feature].value_counts()
+def build_bar_chart(df, target, name='', change_x_ticks=False, angle=30):
+    plt.figure(num=name + target)
+    plt.suptitle(name + target)
+    counts = df[target].value_counts()
     plt.bar(counts.index, counts.values)
-    plt.xticks(rotation=30)
+    x_ticks_mas = []
+    if change_x_ticks:
+        for i in numpy.arange(0, 1, 0.1):
+            x_ticks_mas.append(counts.index[int(len(counts.index) * i)])
+            plt.xticks(x_ticks_mas)
+    plt.xticks(rotation=angle)
 
 
-def scatter_plot_func(df, data, target, name):
+"""
+Функция построения диаграммы рассеяния
+"""
+def scatter_plot_func(df, data_num, target, name):
+    pca = PCA(n_components=2)
+    data = pca.fit_transform(data_num)
     fig = plt.figure()
     fig.suptitle(name)
     labels = df[target].unique()
@@ -38,3 +56,178 @@ def scatter_plot_func(df, data, target, name):
     plt.xlabel("component1")
     plt.ylabel("component2")
     plt.legend(plots, labels, loc="lower right", title="species")
+
+
+"""
+Функция применяющая один из методов классификации и возвращающая процент верного предсказания
+модели на тестовой выборке
+"""
+def apply_clustering_method(model, X_train, y_train, X_test, y_test, result_table, time_result_table, it, type_data):
+    degree = 4
+    polynom_trans = PolynomialFeatures(degree=degree, include_bias=True)
+    print(X_train.shape)
+    x2_train = polynom_trans.fit_transform(X=X_train)
+    x2_test = polynom_trans.transform(X=X_test)
+    pca = PCA(n_components=3)
+
+    x_train_GrLivArea = X_train['GrLivArea']
+    x_test_GrLivArea = X_test['GrLivArea']
+
+    # Посморим на данные
+    plots = []
+    plt.ion()
+    labels = ['train', 'test']
+    fig, ax = plt.subplots()
+    ax.set_title('База данных')
+    ax.scatter(X_train[2], y_train, c='b', edgecolors='k', linewidths=1)
+    ax.scatter(X_test[2], y_test, c='r', edgecolors='k', linewidths=1)
+    ax.set(xlabel='x', ylabel='y')
+    ax.legend(labels, loc="lower right", title="sets", prop={'size': 10})
+    fig.canvas.draw_idle()
+    plt.pause(1)
+
+    # Обучение модели
+    studying_time_start = timer()
+    print('x2_train', x2_train)
+    print('y_train', y_train)
+    model.fit(x2_train, y_train)
+    studying_time_stop = timer()
+
+    # Предсказание
+    predict_time_start = timer()
+    score_train = model.score(X=x2_train, y=y_train)
+    score_test = model.score(X=x2_test, y=y_test)
+    pred_train = model.predict(X=x2_train)
+    # pred_test = model.predict(X=x2_test)
+    mse_train = mean_squared_error(y_true=y_train, y_pred=pred_train)
+    # mse_test = mean_squared_error(y_true=y_test, y_pred=pred_test)
+
+    print('r2 train: {:.3f}'.format(score_train))
+    print('r2 test: {:.3f}'.format(score_test))
+    print('mse train: {:.3f}'.format(mse_train))
+    # print('mse test: {:.3f}'.format(mse_test))
+    predict_time_stop = timer()
+    df = pandas.DataFrame({'Actual': y_train, 'Predicted': pred_train})
+    print(str(model), '  ', type_data, '  IT: ', it)
+    print(df)
+
+    ax.set_title('[Polynomial] degree={}, fit_intercept={}\n w={}, bias={}'.format(degree,
+                                                                                   model.fit_intercept,
+                                                                                   numpy.round(model.coef_, 4),
+                                                                                   numpy.round(model.intercept_, 4)))
+
+    # ax.plot(X_train, pred_train, color='k', linewidth=3)
+    ax.plot(X_train[2], pred_train, 'g.')
+
+    plt.ioff()
+    plt.show()
+    # Оценка верного угадывания модели
+    true_pred = 0
+    for i in range(len(df)):
+        if df.at[i, 'Actual'] == df.at[i, 'Predicted']:
+            true_pred = true_pred + 1
+    # формирование записи в таблице для анализа
+    # label = str(model)[:str(model).find('(')]
+    # result_table.loc[it, label] = true_pred / len(df)
+    # result_table.loc[it, 'type'] = type_data
+    # time_result_table.loc[it, (label + ' ' + 'studying_time')] = studying_time_stop - studying_time_start
+    # time_result_table.loc[it, (label + ' ' + 'predict_time')] = predict_time_stop - predict_time_start
+    # time_result_table.loc[it, 'type'] = type_data
+    # print('alg: ', label, '  type = ', type_data, '  % = ', true_pred / len(df))
+
+
+"""
+Фукнция, разбивающая датафрем на несколько наборов тренеровочных и тестовых фреймов,
+применяющая эти фреймы к раличным методам классификации
+"""
+def get_analiz(data, df_target, result_table, time_result_table, type_data, num_meth):
+    kf = KFold(n_splits=2, shuffle=True, random_state=12)
+    for ikf, (train_index, test_index) in enumerate(kf.split(data)):
+        X_train, X_test = data.values[train_index], data.values[test_index]
+        y_train, y_test = df_target.values[train_index], df_target.values[test_index]
+
+        # print('IT = ', ikf)
+        # print('num_neth = ', num_meth)
+        # LinearRegression(),
+        apply_clustering_method(ElasticNet(alpha=0.5, fit_intercept=False),
+                                X_train, y_train, X_test, y_test, result_table, time_result_table,
+                                ikf + num_meth, type_data)
+
+        # apply_clustering_method(GaussianNB(var_smoothing=0.075),
+        #                         X_train, y_train, X_test, y_test, result_table, time_result_table,
+        #                         5*ikf + num_meth, type_data)
+        #
+        # apply_clustering_method(DecisionTreeClassifier(criterion='gini', min_samples_split=10, max_depth=20),
+        #                         X_train, y_train, X_test, y_test, result_table, time_result_table,
+        #                         5*ikf + num_meth, type_data)
+        #
+        # apply_clustering_method(Pipeline([("scaller", StandardScaler()), ("svm_clf", SVC(kernel="rbf", gamma=3, C=10))]),
+        #                         X_train, y_train, X_test, y_test, result_table, time_result_table,
+        #                         5*ikf + num_meth, type_data)
+        #
+        # apply_clustering_method(RandomForestClassifier(n_estimators=40, criterion='gini', min_samples_split=6, max_depth=40),
+        #                         X_train, y_train, X_test, y_test, result_table, time_result_table,
+        #                         5*ikf + num_meth, type_data)
+        # result_table.loc[5 * ikf+num_meth, 'it'] = ikf
+        # time_result_table.loc[5 * ikf + num_meth, 'it'] = ikf
+
+        # Жирный кусок для автоматического подбора параметров
+        # для всех вышеприменненных методов классификации
+        # print('num_meth = ', num_meth, '  It = ', ikf)
+        #
+        # grid_param = {'n_neighbors': list(range(2, 100)),
+        #                             'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute'],
+        #                             'p': [1, 2, 3]}
+        # grid_search = RandomizedSearchCV(KNeighborsClassifier(), param_distributions=grid_param, n_iter=30)
+        # grid_search.fit(X_train, y_train)
+        # print('type = ', type_data, '  methot = ', str(KNeighborsClassifier()))
+        # print(grid_search.best_params_)
+        # print(grid_search.best_score_)
+        #
+        # grid_param = {'var_smoothing': list(numpy.arange(0, 1, 0.01))}
+        # grid_search = RandomizedSearchCV(GaussianNB(), param_distributions=grid_param, n_iter=30)
+        # grid_search.fit(X_train, y_train)
+        # print('type = ', type_data, '  methot = ', str(GaussianNB()))
+        # print(grid_search.best_params_)
+        # print(grid_search.best_score_)
+        #
+        # grid_param = {'criterion': ['gini', 'entropy'],
+        #               'min_samples_split': list(range(2, 12, 1)),
+        #               'max_depth': list(range(1, 100, 1))}
+        # grid_search = RandomizedSearchCV(DecisionTreeClassifier(), param_distributions=grid_param, n_iter=30)
+        # grid_search.fit(X_train, y_train)
+        # print('type = ', type_data, '  methot = ', str(DecisionTreeClassifier()))
+        # print(grid_search.best_params_)
+        # print(grid_search.best_score_)
+        #
+        # grid_param = {'kernel': ["rbf"],
+        #               'gamma': numpy.arange(0.1, 5, 0.1),
+        #               'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000]}
+        # grid_search = RandomizedSearchCV(SVC(), param_distributions=grid_param, n_iter=30)
+        # grid_search.fit(X_train, y_train)
+        # print('type = ', type_data, '  methot = ', str(SVC()), '  rbf')
+        # print(grid_search.best_params_)
+        # print(grid_search.best_score_)
+        #
+        # if num_meth > 0:
+        #     grid_param = {'loss': ["squared_hinge"],
+        #                   'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+        #                   'max_iter': [1000000],
+        #                   'dual': [False],
+        #                   'penalty': ['l1']}
+        #     grid_search = RandomizedSearchCV(LinearSVC(),
+        #                                      param_distributions=grid_param, n_iter=7)
+        #     grid_search.fit(X_train, y_train)
+        #     print('type = ', type_data, '  methot = ', str(LinearSVC()))
+        #     print(grid_search.best_params_)
+        #     print(grid_search.best_score_)
+        #
+        # grid_param = {'n_estimators': range(2, 50, 2),
+        #               'criterion': ['gini', 'entropy'],
+        #               'min_samples_split': list(range(2, 12, 1)),
+        #               'max_depth': list(range(1, 100, 1))}
+        # grid_search = RandomizedSearchCV(RandomForestClassifier(), param_distributions=grid_param, n_iter=30)
+        # grid_search.fit(X_train, y_train)
+        # print('type = ', type_data, '  methot = ', str(RandomForestClassifier()))
+        # print(grid_search.best_params_)
+        # print(grid_search.best_score_)
