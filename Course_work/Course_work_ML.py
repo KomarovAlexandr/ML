12971d5e5bb0e85f5eee9f524fec4a90e465c4fa
Александр Комаров <raptor.al.com@yandex.ru>
2020-12-18 12:47:56 +0300

    прогнал RandomizedSearchCV на курсаче

--- Course_work/Course_work_ML.py
+++ Course_work/Course_work_ML.py
@@ -1,7 +1,9 @@
 import pandas, numpy
 import matplotlib.pyplot as plt
 from pandas.plotting import scatter_matrix
-DataFrame = pandas.read_csv('Civ5 Leader Bias.csv')
+import additional_func as af
+from sklearn import preprocessing
+data_frame = pandas.read_csv('Civ5 Leader Bias.csv')
 pandas.set_option('display.max_columns', None)
 
 # Запись все параметров во внешний файл
@@ -11,8 +13,8 @@ pandas.set_option('display.max_columns', None)
 # pandas.reset_option('max_columns')
 
 # Поворачиваем таблицу и устанавливаем индекс
-DataFrame = DataFrame.set_index('Unnamed: 0')
-DataFrame = DataFrame.transpose()
+data_frame = data_frame.set_index('Unnamed: 0')
+data_frame = data_frame.transpose()
 
 # Выбор признаков касаемых добычи ресурсов
 resource_signs = ['Archaeology', 'Expansion', 'Gold', 'Great People', 'Growth', 'Happiness', 'Infrastructure',
@@ -25,22 +27,24 @@ character_signs = ['Boldness', 'Chattiness', 'Denounce Willingness', 'Diplomatic
 # Предсказываемые параметры
 Predicted_parameters = ['Culture', 'Diplomacy', 'Spaceship']
 
-DataFrame[Predicted_parameters].hist()
-scatter_matrix(DataFrame[resource_signs])
+data_frame[Predicted_parameters].hist()
+scatter_matrix(data_frame[resource_signs])
 param = ['Culture', 'Diplomacy', 'Spaceship', 'Gold', 'Growth','Infrastructure', 'Production', 'Religion', 'Science', 'Boldness', 'War']
-scatter_matrix(DataFrame[param])
+scatter_matrix(data_frame[param])
 
-def build_gistogramm(feature):
-    plt.figure(num='plt.bar result ' + feature)
-    Values = DataFrame[feature].value_counts()
-    plt.bar(Values.index, Values.values)
-    plt.xticks(rotation=30)
+corr = data_frame[['Spaceship'] + data_frame.columns.to_list()].corr().iloc[0]
+corr.sort_values().to_excel('correlation.xlsx')
+# def build_gistogramm(feature):
+#     plt.figure(num='plt.bar result ' + feature)
+#     Values = DataFrame[feature].value_counts()
+#     plt.bar(Values.index, Values.values)
+#     plt.xticks(rotation=30)
 
-Predicted_parameter = DataFrame['Spaceship']
+Predicted_parameter = data_frame['Diplomacy']
 
 # Метод главных компонент
 from sklearn.decomposition import PCA
-data_num = DataFrame[resource_signs + character_signs]
+data_num = data_frame[resource_signs + character_signs]
 pca = PCA(n_components=2)
 pca.fit(data_num)
 data_reduced = pca.transform(data_num)
@@ -53,7 +57,7 @@ data_reduced = pca.transform(data_num)
 from matplotlib import cm
 import numpy
 fig = plt.figure()
-fig.suptitle("Scatter plot normalize")
+fig.suptitle("Scatter plot")
 plots = []
 labels = numpy.sort(Predicted_parameter.unique())
 colors = cm.rainbow(numpy.linspace(0, 1, len(labels)))
@@ -67,26 +71,51 @@ plt.xlabel("component1")
 plt.ylabel("component2")
 plt.legend(plots, labels, loc="lower right", title="species")
 
-# from sklearn.model_selection import train_test_split
-# X_train, X_test, y_train, y_test = train_test_split(data_reduced, Predicted_parameter, test_size=0.2, random_state=42)
-#
-# from sklearn.linear_model import LinearRegression
-# from sklearn.linear_model import Ridge
-# lin_reg = Ridge(alpha=1, solver="cholesky")
-# lin_reg.fit(X_train, y_train)
-#
-# y_pred = lin_reg.predict(X_test)
-#
-# df = pandas.DataFrame({'Actual': y_test, 'Predicted': y_pred})
-# print("result: ")
-# print(df)
-#
-# plt.figure(num="name")
-# plt.plot(X_train, y_train, "b.")
-# plt.plot(X_test, y_pred, "r-")
-#
-# print(lin_reg.intercept_, lin_reg.coef_)
-# print(lin_reg.predict(X_test))
-# print(y_test[:50])
-#
+
+# ИТОГОВЫЙ НАБОР ПРИЗНАКОВ
+# for Culture
+# attributes = ['Expansion', 'Military Training', 'Victory Competitiveness', 'War', 'Gold', 'CS Bully',
+#               'Growth', 'Warmonger Hatred', 'Guarded', 'Religion', 'Friendly', 'CS Protect']
+# for Diplomacy
+attributes = ['Air', 'Naval Tile Improvement', 'Chattiness', 'Meanness', 'Gold',
+              'Build Nuke', 'Deceptive', 'Neediness', 'Guarded', 'Wonder', 'Diplomatic Balance', 'Science']
+# for Spaceship
+# attributes = ['Guarded', 'Wonder', 'Diplomatic Balance', 'Science',
+#               'Air', 'Naval Tile Improvement', 'Chattiness', 'Meanness', 'Gold']
+
+# ДАТАФРЕЙМ СО ВСЕМИ ЧИСЛЕННЫМИ ПРИЗНАКАМИ
+data_num = data_frame[attributes]
+# ЦЕЛЕВОЙ ПРИЗНАК
+df_target = data_frame['Diplomacy']
+
+# таблицы для записи итоговых результатов
+result = pandas.DataFrame()
+time_result = pandas.DataFrame()
+
+# Оригинальные данные
+af.get_analiz(data_num, df_target, result, time_result, 'original', 0)
+af.scatter_plot_func(data_frame, data_num, 'Culture', "ORIGINAL")
+# Стандартизация
+df_stand = preprocessing.scale(data_num)
+df_stand = pandas.DataFrame(data=df_stand, index=data_num.index, columns=attributes)
+af.get_analiz(df_stand, df_target, result, time_result, 'standardization', 1)
+af.scatter_plot_func(data_frame, df_stand, 'Culture', "STANDARDIZATION")
+# Нормализация
+df_norm = preprocessing.normalize(data_num)
+df_norm = pandas.DataFrame(data=df_norm, index=data_num.index, columns=attributes)
+af.get_analiz(df_norm, df_target, result, time_result, 'normalization', 2)
+af.scatter_plot_func(data_frame, df_norm, 'Culture', "NORMALIZATION")
+
+# обновление индексации таблиц для корректного отображения
+result = result.sort_values(['it']).set_index(['it', 'type'], drop=True)
+time_result = time_result.sort_values(['it']).set_index(['it', 'type'], drop=True)
+
+# Вывод в таблицы excel
+result.to_excel('result.xlsx')
+time_result.to_excel('time_result.xlsx')
+
+print(result)
+print(time_result)
+
 plt.show()
+pandas.reset_option('max_columns')
