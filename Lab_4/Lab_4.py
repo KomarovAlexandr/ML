ed983f8203118b82a2b8a4447268a4eebcf7338e
Александр Комаров <raptor.al.com@yandex.ru>
2021-01-30 13:26:04 +0300

    работают 4 метода. Добавлена функция отображения от Никиты

--- Lab_4/Lab_4.py
+++ Lab_4/Lab_4.py
@@ -2,12 +2,7 @@ import pandas, seaborn, numpy
 import matplotlib.pyplot as plt
 import additional_func as af
 from sklearn import preprocessing
-from pandas.plotting import scatter_matrix
 from sklearn.decomposition import PCA
-from sklearn.cluster import DBSCAN
-from sklearn.cluster import KMeans
-from sklearn import decomposition
-from sklearn import metrics
 
 import warnings
 warnings.simplefilter('ignore')
@@ -18,7 +13,7 @@ DataFrame = pandas.read_csv('train.csv')
 # Получение таблицы с признаками, их типом, максимальным и минимальным значениями
 DataFrame.describe().to_excel('data_describe.xlsx')
 
-DataFrame = DataFrame[(DataFrame[["Id"]] % 2 == 0).all(axis=1)]
+# DataFrame = DataFrame[(DataFrame[["Id"]] % 2 == 0).all(axis=1)]
 
 # Удаление столбцов с большим кол-вом пустых значений
 DataFrame = DataFrame.drop(['PoolQC', 'Alley', 'FireplaceQu', 'Fence', 'MiscFeature', 'LotFrontage'], axis=1)
@@ -31,6 +26,10 @@ DataFrame = DataFrame[(DataFrame[["GrLivArea"]] < 2500).all(axis=1)]
 DataFrame = DataFrame[(DataFrame[["TotalBsmtSF"]] < 2000).all(axis=1)]
 DataFrame = DataFrame[(DataFrame[["TotalBsmtSF"]] > 450).all(axis=1)]
 DataFrame = DataFrame[(DataFrame[["GarageArea"]] < 1000).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["SalePrice"]] < 300000).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["YearBuilt"]] > 1910).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["GrLivArea"]] > 800).all(axis=1)]
+DataFrame = DataFrame[(DataFrame[["GrLivArea"]] < 2200).all(axis=1)]
 
 # Добавление признака с ценой за один квадратный фут жилой площади
 DataFrame.loc[:, 'PricePerFoot'] = DataFrame['SalePrice'] / DataFrame['GrLivArea']
@@ -50,14 +49,20 @@ corr = DataFrame[['SalePrice'] + DataFrame.columns.to_list()].corr().iloc[0]
 corr.sort_values().to_excel('correlation.xlsx')
 
 # Разбиение цены на категориальный признак
-labels = [1, 2, 3]
+labels = [1, 0, 2, 0, 3]
+delta_price = (DataFrame['SalePrice'].max() - DataFrame['SalePrice'].min()) / 5
+print(DataFrame['SalePrice'].min(), ' ', delta_price, ' ', DataFrame['SalePrice'].max())
 price_group = pandas.cut(DataFrame['SalePrice'],
                     bins=[DataFrame['SalePrice'].min(),
-                          145000,
-                          200000,
+                          DataFrame['SalePrice'].min() + 2.0*delta_price,
+                          DataFrame['SalePrice'].min() + 2.2*delta_price,
+                          DataFrame['SalePrice'].min() + 2.8*delta_price,
+                          DataFrame['SalePrice'].min() + 3.0*delta_price,
                           DataFrame['SalePrice'].max()],
-                    labels=labels)
+                    labels=labels, ordered=False)
 DataFrame.loc[:, 'price_group'] = numpy.array(price_group)
+DataFrame = DataFrame[(DataFrame[['price_group']] > 0).all(axis=1)]
+DataFrame.reset_index(drop=True)
 
 # Дополнительное удаление пустых строк. Почему то это нужно сделать еще раз, иначе бо-бо
 DataFrame = DataFrame.dropna()
@@ -69,13 +74,17 @@ attributes = ['PricePerFoot', 'OverallQual', 'GrLivArea', 'YearBuilt', 'SalePric
 # ДАТАФРЕЙМ СО ВСЕМИ ЧИСЛЕННЫМИ ПРИЗНАКАМИ
 data_num = DataFrame[attributes]
 
+# Вывод всех гистограмм
+# for i in attributes + ['price_group']:
+#     af.build_bar_chart(DataFrame, i, i, )
+
 # Построение карты корреляций
 plt.figure()
-corr = DataFrame[attributes + ['SalePrice']].corr()
+corr = DataFrame.corr()
 sns_hmap = seaborn.heatmap(abs(corr))
 sns_hmap.set_title("correlation PANDAS + SEABORN")
-corr = DataFrame[attributes + ['SalePrice']].corr().iloc[0]
-corr.sort_values().to_excel('corr.xlsx')
+# corr = DataFrame.corr().iloc[0]
+corr.to_excel('corr.xlsx')
 
 # таблицы для записи итоговых результатов
 result = pandas.DataFrame()
@@ -84,123 +93,44 @@ time_result = pandas.DataFrame()
 # ОБУЧЕНИЕ МОДЕЛЕЙ
 # Оригинальные данные
 af.scatter_plot_func(DataFrame, data_num, 'price_group', "ORIGINAL")
-# Стандартизация
-df_stand = preprocessing.scale(data_num)
-df_stand = pandas.DataFrame(data=df_stand, index=data_num.index, columns=attributes)
-af.scatter_plot_func(DataFrame, df_stand, 'price_group', "STANDARDIZATION")
 # Нормализация
 df_norm = PCA(n_components=2).fit_transform(data_num)
 df_norm = preprocessing.normalize(data_num)
 df_norm = pandas.DataFrame(data=df_norm, index=data_num.index, columns=attributes)
 af.scatter_plot_func(DataFrame, df_norm, 'price_group', "NORMALIZATION")
 
-"""
-data = df_norm.to_numpy()
-# data.loc[:, 'price_group'] = data_num['price_group']
-
-plt.figure()
-db = DBSCAN(eps=0.01, min_samples=10).fit(data)
-core_samples_mask = numpy.zeros_like(db.labels_, dtype=bool)
-core_samples_mask[db.core_sample_indices_] = True
-labels = db.labels_
-
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-n_noise_ = list(labels).count(-1)
-
-print("data size = ", len(data_num))
-print('Estimated number of clusters: %d' % n_clusters_)
-print('Estimated number of noise points: %d' % n_noise_)
-
-unique_labels = set(labels)
-colors = [plt.cm.Spectral(each)
-          for each in numpy.linspace(0, 1, len(unique_labels))]
-for k, col in zip(unique_labels, colors):
-    if k == -1:
-        # Black used for noise.
-        col = [0, 0, 0, 1]
-
-    class_member_mask = (labels == k)
-
-    xy = data[class_member_mask & core_samples_mask]
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=14)
-
-    xy = data[class_member_mask & ~core_samples_mask]
-    print('xy = ', len(xy))
-    plt.plot(xy[:, 0], xy[:, 1], 'o', markerfacecolor=tuple(col),
-             markeredgecolor='k', markersize=6)
-
-plt.title('Estimated number of clusters: %d' % n_clusters_)
-"""
-"""
-from scipy.cluster.hierarchy import linkage, dendrogram
-# Извлекаем измерения как массив NumPy
-samples = df_norm.values
-
-# Реализация иерархической кластеризации при помощи функции linkage
-mergings = linkage(samples, method='complete')
-
-#varieties = list(df_norm.pop('price_group'))
-
-# Строим дендрограмму, указав параметры удобные для отображения
-dendrogram(mergings,
-           #labels=varieties,
-           leaf_rotation=90,
-           leaf_font_size=1,
-           )
-"""
 
 reduced_data = PCA(n_components=2).fit_transform(data_num)
-
 plt.figure()
 plt.scatter(reduced_data[:, 0], reduced_data[:, 1])
+labels_true = DataFrame['price_group']
 
 # reduced_data = df_stand.to_numpy()
-kmeans = KMeans(init='k-means++', n_clusters=3, n_init=10)
-kmeans.fit(reduced_data)
+'''--------------------------K-means-----------------------------------------'''
+from sklearn.cluster import KMeans
+kmeans = KMeans(init='k-means++', n_clusters=3, n_init=50)
+af.show_klasters(reduced_data, kmeans, DataFrame['price_group'], 'K-means')
 
-labels = kmeans.labels_
-print("silhouette_score ", metrics.silhouette_score(reduced_data, labels, metric='euclidean'))
-print("davies_bouldin_score ", metrics.davies_bouldin_score(reduced_data, labels))
 
-h = 10
-# Граничные значения и значения сетки
-x_min, x_max = reduced_data[:, 0].min() - 1, reduced_data[:, 0].max() + 1
-y_min, y_max = reduced_data[:, 1].min() - 1, reduced_data[:, 1].max() + 1
-xx, yy = numpy.meshgrid(numpy.arange(x_min, x_max, h), numpy.arange(y_min, y_max, h))
-# Получим результат для каждой точки сетки и выведем диаграмму
-Z = kmeans.predict(numpy.c_[xx.ravel(), yy.ravel()])
-Z = Z.reshape(xx.shape)
-plt.figure(1)
-plt.clf()
-plt.imshow(Z, interpolation='nearest',
-           extent=(xx.min(), xx.max(), yy.min(), yy.max()),
-           cmap=plt.cm.Paired,
-           aspect='auto', origin='lower')
+'''-----------------------GaussianMixture-----------------------------------'''
+from sklearn.mixture import GaussianMixture
+model_GaussianMixture = GaussianMixture(n_components=3, covariance_type='spherical', tol=1e-3, reg_covar=1e-6, max_iter=1000,
+                                        init_params='kmeans', random_state=0, n_init=5)
+af.show_klasters(reduced_data, model_GaussianMixture, DataFrame['price_group'], 'GaussianMixture')
 
-plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
-# Построим центроиды (центры кластеров) на диаграмме в виде крестиков
-centroids = kmeans.cluster_centers_
-plt.scatter(centroids[:, 0], centroids[:, 1],
-            marker='x', s=169, linewidths=3,
-            color='w', zorder=10)
-plt.title('K-means')
-plt.xlim(x_min, x_max)
-plt.ylim(y_min, y_max)
-plt.xticks(())
-plt.yticks(())
 
-#
-# # обновление индексации таблиц для корректного отображения
-# result = result.sort_values(['it']).set_index(['it', 'type'], drop=True)
-# time_result = time_result.sort_values(['it']).set_index(['it', 'type'], drop=True)
-#
-# # Вывод в таблицы excel
-# result.to_excel('result.xlsx')
-# time_result.to_excel('time_result.xlsx')
-#
-# print(result)
-# print(time_result)
+'''-------------------------DBSCAN--------------------------------------'''
+from sklearn.cluster import DBSCAN
+model_dbscan = DBSCAN(eps=8000, min_samples=2)
+af.show_klasters(reduced_data, model_dbscan, DataFrame['price_group'], 'DBSCAN')
+
+
+'''-------------------------SpectralClustering--------------------------------------'''
+from sklearn.cluster import SpectralClustering
+reduced_data = PCA(n_components=2).fit_transform(df_norm)
+model_spectClust = SpectralClustering(n_clusters=3, assign_labels="kmeans", gamma=10,
+                                      random_state=25, n_neighbors=8, affinity='nearest_neighbors')
+af.show_klasters(reduced_data, model_spectClust, DataFrame['price_group'], 'SpectralClustering')
 
 plt.show()
 pandas.reset_option('max_columns')
--- Lab_4/additional_func.py
+++ Lab_4/additional_func.py
@@ -1,19 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy, pandas
-from timeit import default_timer as timer
 from matplotlib import cm
-from sklearn.model_selection import KFold
 from sklearn.decomposition import PCA
-from sklearn.linear_model import LinearRegression, ElasticNet
-from sklearn.kernel_ridge import KernelRidge
-from sklearn.metrics import mean_squared_error
-from sklearn.metrics import mean_absolute_error
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.metrics import r2_score
-from sklearn.pipeline import make_pipeline
+
 import warnings
 warnings.simplefilter('ignore')
 from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
 
 """
 Построение гистограммы распределения по входному признаку датафрейма
@@ -53,79 +46,66 @@ def scatter_plot_func(df, data_num, target, name):
 
 
 """
-Функция применяющая один из методов регрессии и возвращающая процент верного предсказания
-модели на тестовой выборке
+Визуализация данных
 """
-def apply_regression_method(model, X_train, y_train, X_test, y_test, result_table, time_result_table, it, type_data):
-    # Обучение модели
-    studying_time_start = timer()
-    model.fit(X_train, y_train)
-    studying_time_stop = timer()
-    # Предсказание
-    predict_time_start = timer()
-    pred_y = model.predict(X=X_test)
-    predict_time_stop = timer()
+def show_klasters(reduced_data, model, labels_true, method):
+    print("-------", method, "--------")
+    labels = model.fit_predict(reduced_data)
+    print("silhouette_score ", metrics.silhouette_score(reduced_data, labels, metric='euclidean'))
+    print("davies_bouldin_score ", metrics.davies_bouldin_score(reduced_data, labels))
+    print("adjusted_rand_score", metrics.adjusted_rand_score(labels_true, labels))
 
-    # Параметры и метрики модели
-    score_test = r2_score(y_test, pred_y)
-    mse_test = mean_squared_error(y_true=y_test, y_pred=pred_y)
-    mae_test = mean_absolute_error(y_true=y_test, y_pred=pred_y)
-    # Расчет средней относительной ошибки
-    mre = (abs(y_test - pred_y) / y_test).mean()
-    mae_pr = mae_test / y_test.mean()
+    h_x = (reduced_data[:, 0].max() - reduced_data[:, 0].min()) / 200
+    h_y = (reduced_data[:, 1].max() - reduced_data[:, 1].min()) / 200
+    # Граничные значения и значения сетки
+    x_min, x_max = reduced_data[:, 0].min() - h_x, reduced_data[:, 0].max() + h_x
+    y_min, y_max = reduced_data[:, 1].min() - h_y, reduced_data[:, 1].max() + h_y
+    xx, yy = numpy.meshgrid(numpy.arange(x_min, x_max, h_x), numpy.arange(y_min, y_max, h_y))
 
-    # формирование записи в таблице для анализа
-    label = str(model)[:str(model).find('(')]
-    result_table.loc[it, label + ' r2'] = score_test
-    result_table.loc[it, label + ' mse'] = mse_test
-    result_table.loc[it, label + ' mae'] = mae_test
-    result_table.loc[it, label + ' mae %'] = mae_pr
-    result_table.loc[it, label + ' mre'] = mre
+    # Получим результат для каждой точки сетки и выведем диаграмму
+    try:
+        Z = model.predict(numpy.c_[xx.ravel(), yy.ravel()])
+        Z = Z.reshape(xx.shape)
+        plt.figure()
+        plt.clf()
+        plt.imshow(Z, interpolation='nearest',
+                   extent=(xx.min(), xx.max(), yy.min(), yy.max()),
+                   cmap=plt.cm.Paired,
+                   aspect='auto', origin='lower')
 
-    result_table.loc[it, 'type'] = type_data
+        plt.plot(reduced_data[:, 0], reduced_data[:, 1], 'k.', markersize=2)
+        # Построим центроиды (центры кластеров) на диаграмме в виде крестиков
+        try:
+            centroids = model.cluster_centers_
+            plt.scatter(centroids[:, 0], centroids[:, 1],
+                        marker='x', s=169, linewidths=3,
+                        color='w', zorder=10)
+        except:
+            print("no centroids for ", method)
 
-    time_result_table.loc[it, (label + ' ' + 'studying_time')] = studying_time_stop - studying_time_start
-    time_result_table.loc[it, (label + ' ' + 'predict_time')] = predict_time_stop - predict_time_start
-    time_result_table.loc[it, 'type'] = type_data
-    # print('it = ', it, '   label = ', label, '   type = ', type_data)
+        plt.title(method)
+        plt.xlim(x_min, x_max)
+        plt.ylim(y_min, y_max)
+        plt.xticks(())
+        plt.yticks(())
+    except:
+        fig = plt.figure()
+        # plt.scatter(reduced_data[:, 0], reduced_data[:, 1], )
+        fig.suptitle(method)
+        plt.scatter(x=reduced_data[:, 0], y=reduced_data[:, 1], c=labels)
+        # labels_unique = numpy.unique(labels, return_counts=False)
+        # labels = labels.unique()
+        # plots = []
+        # colors = cm.rainbow(numpy.linspace(0, 1, len(labels_unique)))
+        # for ing, ng in enumerate(labels_unique):
+        #     plots.append(plt.scatter(x=reduced_data[ng, 0],
+        #                              y=reduced_data[ng, 1],
+        #                              c=colors[ing],
+        #                              edgecolor='k'))
+        # plt.xlabel("component1")
+        # plt.ylabel("component2")
+        # plt.legend(plots, labels, loc="lower right", title="species")
 
-
-"""
-Функция осущствляющая три полных перебора набора параметров grid_param для метода регрессии 
-estimator и оценивающий результат по трем метрикам: r2, средней квадратичной ошибки и средней
-абсолютной ошибки соответсвенно
-"""
-def use_grid_search(X_train, y_train, estimator, grid_param, type_data):
-    print('type = ', type_data, '  method = ', str(estimator))
-    # Перебор для оценки по метрике r2
-    grid_search = GridSearchCV(estimator=estimator, param_grid=grid_param,
-                               scoring='r2', cv=5, pre_dispatch=1, n_jobs=1)
-    grid_search.fit(X_train, y_train)
-    print('metric: ', 'r2')
-    print(grid_search.best_params_)
-    print(grid_search.best_score_)
-
-
-"""
-Фукнция, разбивающая датафрем на несколько наборов тренеровочных и тестовых фреймов,
-применяющая эти фреймы к раличным методам классификации
-"""
-def get_analiz(data, df_target, result_table, time_result_table, type_data, num_meth):
-    # Жирный кусок для автоматического подбора параметров
-    # для всех используемых методов регрессии
-    # X_train = data.values
-    # y_train = df_target.values
-    # grid_param = {'alpha': list(numpy.arange(0.05, 1, 0.05)),
-    #               'fit_intercept': ['False', 'True']}
-    # use_grid_search(X_train, y_train, ElasticNet(), grid_param, type_data)
-
-
-    X_train, X_test = data.values[train_index], data.values[test_index]
-    y_train, y_test = df_target.values[train_index], df_target.values[test_index]
-
-    apply_regression_method(ElasticNet(alpha=0.05, fit_intercept=True, normalize=False),
-                            X_train, y_train, X_test, y_test, result_table, time_result_table,
-                            3*ikf + num_meth, type_data)
 
 
 
